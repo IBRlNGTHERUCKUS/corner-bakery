8a098a70ed7d6e4e0b9a74a27762ab8555850242
Joseph C <josephalexcobian@gmail.com>
2023-12-04 20:50:49 -0800

    Create function to generate the menu

---
+++ main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const content = document.querySelector(".content");
+content.textContent = 'Hello World';
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0Esb0MiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jb3JuZXItYmFrZXJ5Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGNvbnRlbnQgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKFwiLmNvbnRlbnRcIik7XG5jb250ZW50LnRleHRDb250ZW50ID0gJ0hlbGxvIFdvcmxkJzsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
