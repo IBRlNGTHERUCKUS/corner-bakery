421ac3d8c24ac5d4f9886e009681f07bf7a00dbf
Joseph C <josephalexcobian@gmail.com>
2023-12-04 21:52:06 -0800

    Modify json structure and genMenu to put items into sections

--- src/genMenu.js
+++ src/genMenu.js
@@ -8,26 +8,35 @@ function generateMenu(menuItems) {
     content.append(hero);
     // Menu items
     const menu = document.createElement('div');
-    for (let item of menuItems) {
-        // Create the necessary elements
-        let itemContainer = document.createElement('div');
-        let itemName = document.createElement('h2');
-        let itemImage = document.createElement('img');
-        let itemDesc = document.createElement('p');
-        let itemPrice = document.createElement('p');
-        // Assign classes
-        itemContainer.classList.add('item-container');
-        itemName.classList.add('item-name');
-        itemImage.classList.add('item-image');
-        itemDesc.classList.add('item-description');
-        itemPrice.classList.add('item-price');
-        // Fill data
-        itemName.textContent = item.name;
-        itemDesc.textContent = item.desc;
-        itemPrice.textContent = item.price;
-        // Attach items 
-        itemContainer.append(itemImage, itemName, itemDesc, itemPrice);
-        menu.append(itemContainer);
+    for (let section of menuItems) {
+        // Create section title
+        const sectionName = document.createElement('h1');
+        sectionName.textContent = section.sectionName;
+        menu.append(sectionName)
+        // Create section content
+        const sectionContainer = document.createElement('div');
+        for (let item of section.sectionItems) {
+            // Create the necessary elements
+            let itemContainer = document.createElement('div');
+            let itemName = document.createElement('h2');
+            let itemImage = document.createElement('img');
+            let itemDesc = document.createElement('p');
+            let itemPrice = document.createElement('p');
+            // Assign classes
+            itemContainer.classList.add('item-container');
+            itemName.classList.add('item-name');
+            itemImage.classList.add('item-image');
+            itemDesc.classList.add('item-description');
+            itemPrice.classList.add('item-price');
+            // Fill data
+            itemName.textContent = item.name;
+            itemDesc.textContent = item.desc;
+            itemPrice.textContent = item.price;
+            // Attach items 
+            itemContainer.append(itemImage, itemName, itemDesc, itemPrice);
+            sectionContainer.append(itemContainer);
+        }
+        menu.append(sectionContainer)
     }
     content.append(hero);
     content.append(menu);
